f57bb558ece0e3b26942815ac882341a437ba439
deatheyes <deatheye@yeah.net>
2017-10-24 22:04:21 +0800

    test success

--- src/main/java/org/ksarch/sonar/checks/HystrixCheck.java
+++ src/main/java/org/ksarch/sonar/checks/HystrixCheck.java
@@ -1,13 +1,16 @@
 package org.ksarch.sonar.checks;
 
+import com.sonar.sslr.api.Token;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.sonar.check.Rule;
 import org.sonar.check.RuleProperty;
 import org.sonar.plugins.java.api.JavaFileScanner;
 import org.sonar.plugins.java.api.JavaFileScannerContext;
-import org.sonar.plugins.java.api.tree.BaseTreeVisitor;
-import org.sonar.plugins.java.api.tree.ClassTree;
-import org.sonar.plugins.java.api.tree.TypeTree;
+import org.sonar.plugins.java.api.semantic.Symbol;
+import org.sonar.plugins.java.api.tree.*;
 
+import java.beans.Expression;
 import java.lang.annotation.*;
 import java.lang.reflect.Method;
 import java.util.List;
@@ -15,25 +18,51 @@ import java.util.List;
 
 @Rule(key = "HystrixCheck", name = "Fallback")
 public class HystrixCheck extends BaseTreeVisitor implements JavaFileScanner {
-    @Target(ElementType.METHOD)
-    @Retention(RetentionPolicy.RUNTIME)
-    public @interface HystrixCommand {
-        String fallbackMethod();
-    }
-
     private JavaFileScannerContext context;
     private Boolean implementsHystrix = Boolean.FALSE;
+    private static final Logger LOGGER = LoggerFactory.getLogger(HystrixCheck.class);
 
     public void scanFile(JavaFileScannerContext context) {
         this.context = context;
         scan(context.getTree());
     }
 
+    private Boolean vaildateAnnotation(List<AnnotationTree> annotations, String name, String key) {
+        Boolean retVal = Boolean.TRUE;
+        for (AnnotationTree annotationTree : annotations) {
+            if (annotationTree.annotationType().is(Tree.Kind.IDENTIFIER)) {
+                retVal = Boolean.FALSE;
+                IdentifierTree idf = (IdentifierTree)annotationTree.annotationType();
+                for (ExpressionTree argument : annotationTree.arguments()) {
+                    if (argument.is(Tree.Kind.ASSIGNMENT)) {
+                        AssignmentExpressionTree assignmentExpressionTree = (AssignmentExpressionTree) argument;
+                        IdentifierTree nameTree = (IdentifierTree) assignmentExpressionTree.variable();
+                        if (nameTree.name().equals(key)) {
+                            retVal = Boolean.TRUE;
+                        }
+                    }
+                }
+            }
+        }
+        return retVal;
+    }
+
+    @Override
+    public void visitMethod(MethodTree tree) {
+        List<AnnotationTree> annotations = tree.modifiers().annotations();
+        Boolean retVal = vaildateAnnotation(annotations, "HystrixCommand", "fallbackMethod");
+        if (!retVal) {
+            context.reportIssue(this, tree, String.format("There is no Hystrix fallback method in class @%s", tree.simpleName()));
+        }
+        super.visitMethod(tree);
+    }
+
     @Override
     public void visitClass(ClassTree tree) {
-        if (tree.superClass() != null) {
+        if (((ClassTree) tree).superClass() != null) {
             /* with superclass */
-            String superClassName = tree.superClass().getClass().getName();
+            /* For 'symbolType' usage, jar in dependencies must be on classpath, !unknownSymbol! result otherwise */
+            String superClassName = tree.superClass().symbolType().name();
             if (superClassName.equals("HystrixCommand")) {
                 /* hystrix command, find the method 'getFallback'*/
                 List<TypeTree> interfaces = tree.superInterfaces();
@@ -45,29 +74,18 @@ public class HystrixCheck extends BaseTreeVisitor implements JavaFileScanner {
 
                 if (!implementsHystrix) {
                     /* no fallback found, report the defect */
-                    context.reportIssue(this, tree, String.format("There is no fallback method in class @%s", tree.getClass().getName()));
+                    context.reportIssue(this, tree, String.format("There is no Hystrix fallback method in class @%s", tree.simpleName()));
                 }
             }
         }
 
-        /* possible spring annotation usage */
-        Method[] methods = tree.getClass().getDeclaredMethods();
-        for (Method method : methods) {
-            HystrixCommand hystrixCommand = method.getAnnotation(HystrixCommand.class);
-            if (hystrixCommand == null) {
-                continue;
-            }
-
-            /* Hystrix, check fallback */
-            String fallback = hystrixCommand.fallbackMethod();
-            if (fallback.length() <= 0) {
-                /* no fallback found, report the defect */
-                context.reportIssue(this, tree, String.format("There is no fallback for method @%s", method.getName()));
-            }
+        /* check FeignClient */
+        List<AnnotationTree> annotations = tree.modifiers().annotations();
+        boolean retVal = vaildateAnnotation(annotations, "FeignClient", "fallback");
+        if (!retVal) {
+            context.reportIssue(this, tree, String.format("There is no Hystrix fallback processor for class @%s", tree.simpleName()));
         }
 
-        // The call to the super implementation allows to continue the visit of the AST.
-        // Be careful to always call this method to visit every node of the tree.
         super.visitClass(tree);
     }
 }
